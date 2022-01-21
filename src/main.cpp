5aba8f256a286cf7ce6bbb37d07606b07871b8df
Aryeh <aryeh_zinn@mckinsey.com>
2022-01-21 11:12:56 +1100

    Added integration with volk. Need to test further

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
-#define GLFW_INCLUDE_VULKAN
-#include <vulkan/vulkan.h>
+#define VOLK_IMPLEMENTATION
+#include <volk.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() 
 {
+    VkResult result = volkInitialize();
+
+    VkInstance instance; 
+
+    volkLoadInstance(instance);
+
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Test Window", nullptr, nullptr);
